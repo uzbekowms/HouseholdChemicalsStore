9b55426a64af93ab20f3a75b1e0aa30f7f806a77
Maks <petrunmaks@gmail.com>
2023-05-08 22:21:36 +0300

    new frontend vue npm folder

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    some test changes for push
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
