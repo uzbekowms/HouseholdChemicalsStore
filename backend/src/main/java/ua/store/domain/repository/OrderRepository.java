72e300a3747363e1dfe651072252683c332beef7
uzbekowms <lukasowms@gmail.com>
2023-06-17 20:11:40 +0300

    Vhange query

--- backend/src/main/java/ua/store/domain/repository/OrderRepository.java
+++ backend/src/main/java/ua/store/domain/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package ua.store.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.store.domain.model.Order;
 import ua.store.domain.model.User;
@@ -15,6 +16,6 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findAllByOwner(User owner);
 
     @Modifying
-    @Query("update Order o set o.status.id = :statusId where o.id = :order")
-    void updateStatus(int order, int statusId);
+    @Query("update Order o set o.status.id = :statusId where o.id = :orderId")
+    void updateStatus(@Param("orderId") int orderId, @Param("statusId") int statusId);
 }
