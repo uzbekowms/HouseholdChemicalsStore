78cd882354cab3cc1168ba1b01664e9fce2cf193
uzbekowms <lukasowms@gmail.com>
2023-06-17 16:53:03 +0300

    Change validation from @Size -> @Pattern

--- backend/src/main/java/ua/store/web/dto/Payment.java
+++ backend/src/main/java/ua/store/web/dto/Payment.java
@@ -3,7 +3,7 @@ package ua.store.web.dto;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,10 +12,10 @@ import java.util.Date;
 public class Payment {
 
     @NotBlank(message = "card number cannot be empty")
-    @Size(min = 16, max = 16, message = "Card number is invalid")
+    @Pattern(regexp="^\\d{16}$", message = "Card number is invalid")
     private String cardNumber;
     @NotBlank(message = "cvv cannot be empty")
-    @Size(min = 3, max = 3, message = "Cvv number is invalid")
+    @Pattern(regexp="^\\d{3}$", message = "Cvv is invalid")
     private String cvv;
     @NotBlank(message = "Expiration date cannot be empty")
     @Future(message = "Card has expired")
