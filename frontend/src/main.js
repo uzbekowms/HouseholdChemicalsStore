c2ef8cb009a23a5b99cfe045b5064b5f4f5482a1
uzbekowms <lukasowms@gmail.com>
2023-06-21 04:59:18 +0300

    Move backend to root directory

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '../resources/css/app.css'
-import './assets/tailwind.css'
-const app = createApp(App)
-
-app.use(router)
-app.mount('#app')
